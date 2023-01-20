358a3ed211df3d987bad00d95ab6782f779746fe
Phillip Yen <yphillip@gmail.com>
2023-01-19 17:40:06 -0800

    update rel import

--- goodbudget_cli/run_goodbudget.py
+++ goodbudget_cli/run_goodbudget.py
@@ -14,7 +14,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from utils import format_date
+from .utils import format_date
 
 
 def get_envelope_from_keyword(keyword: str) -> str:
